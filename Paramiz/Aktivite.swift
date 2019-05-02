4d63ab7315a244e9bbd85ad5653c3318266a797a
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-05-02 17:36:45 +0300

    8. Ders

---
+++ Paramiz/Aktivite.swift
@@ -0,0 +1,14 @@
+//
+//  Aktivite.swift
+//  Paramiz
+//
+//  Created by Furkan Sabaz on 2.05.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import Foundation
+
+class Aktivite {
+    var Adi : String = ""
+    var Bittimi : Bool = false
+}
--- Paramiz/AktivitelerVC.swift
+++ Paramiz/AktivitelerVC.swift
@@ -12,11 +12,33 @@ class AktivitelerVC: UITableViewController {
 
     
     var veriler = UserDefaults.standard
-    var aktivitelerListesi = ["Ev","Kapadokya Gezisi","İstanbul Gezisi","Okul Arkadaşları"]
+    //var aktivitelerListesi = ["Ev","Kapadokya Gezisi","İstanbul Gezisi","Okul Arkadaşları"]
+    var aktivitelerListesi = [Aktivite]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        let a1 = Aktivite()
+        a1.Adi = "Ev"
+        a1.Bittimi = true
+        aktivitelerListesi.append(a1)
+        let a2 = Aktivite()
+        a2.Adi = "Kapadokya Gezisi"
+        aktivitelerListesi.append(a2)
+        let a3 = Aktivite()
+        a3.Adi = "İstanbul Gezisi"
+        aktivitelerListesi.append(a3)
+        let a4 = Aktivite()
+        a4.Adi = "Okul Arkadaşları"
+        a4.Bittimi = true
+        aktivitelerListesi.append(a4)
+        
+        for i in 1...100 {
+            let a5 = Aktivite()
+            a5.Adi = "İstanbul Gezisi(\(i))"
+            aktivitelerListesi.append(a5)
+        }
+        
+       /*
         if let liste = veriler.array(forKey: "AktiviteListesi") as? [String] {
             aktivitelerListesi = liste
         }
@@ -24,6 +46,7 @@ class AktivitelerVC: UITableViewController {
         for i in 1...100 {
             aktivitelerListesi.append("\(i)")
         }
+         */
         
         
     }
@@ -42,9 +65,16 @@ class AktivitelerVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let cell = UITableViewCell(style: .default, reuseIdentifier: "aktiviteCell")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "aktiviteCell", for: indexPath)
-        cell.textLabel?.text = aktivitelerListesi[indexPath.row]
+        print("Çalıştı CellForRowAt \(indexPath.row)")
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "aktiviteCell")
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "aktiviteCell", for: indexPath)
+        cell.textLabel?.text = aktivitelerListesi[indexPath.row].Adi
+        
+        if aktivitelerListesi[indexPath.row].Bittimi {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     
@@ -52,13 +82,17 @@ class AktivitelerVC: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let secilenHucre = tableView.cellForRow(at: indexPath)
-       
+        
+        aktivitelerListesi[indexPath.row].Bittimi = !aktivitelerListesi[indexPath.row].Bittimi
+        
+       /*
         if secilenHucre?.accessoryType == UITableViewCell.AccessoryType.checkmark {
             secilenHucre?.accessoryType = .none
         } else {
             secilenHucre?.accessoryType = .checkmark
         }
-        
+         */
+        tableView.reloadData()//cellForRowAt metodu tekrar çalışacak ve bütün satırlar yeniden oluşacak
         
     }
     
@@ -73,7 +107,9 @@ class AktivitelerVC: UITableViewController {
             let txtAktiviteAdi = alertController.textFields![0]
             
             if !txtAktiviteAdi.text!.isEmpty {
-                self.aktivitelerListesi.append(txtAktiviteAdi.text!)
+                let a1 = Aktivite()
+                a1.Adi = txtAktiviteAdi.text!
+                self.aktivitelerListesi.append(a1)
                 self.veriler.set(self.aktivitelerListesi, forKey: "AktiviteListesi")
                 self.tableView.reloadData()
             }
