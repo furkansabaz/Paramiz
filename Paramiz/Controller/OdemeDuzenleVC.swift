e92cea2fb2f21c36093b0960ae1566bfcf691760
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-05-13 10:05:16 +0300

    Ders 17-Ödeme Detay Sayfasının Tasarımı

---
+++ Paramiz/Controller/OdemeDuzenleVC.swift
@@ -0,0 +1,22 @@
+//
+//  OdemeDuzenleVC.swift
+//  Paramiz
+//
+//  Created by Furkan Sabaz on 13.05.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class OdemeDuzenleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
